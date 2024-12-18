5ddf343a96a1146878dd578726652ef876878486
Axaba <lefevre.natacha60@gmail.com>
2024-12-18 01:23:26 +0100

    Création du routeur React et des pages de l'appli

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
+// Point d'entrée de l'appli. Lien entre React et le fichier HTML principal.
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -6,5 +8,5 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
